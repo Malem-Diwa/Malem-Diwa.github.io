a2ee8404f5a13b1568747273de750d5a662c10d8
Malem-Diwa <malem.diwa25@outlook.com>
2026-06-05 18:31:21 +0700

    Added two new programs.
    It's two Construct 2 projects I made when I was just learning Construct 2.
    Only "Simple Platformer" has its own Page for information, for now.
    As for "It moves", you can only play it by directly typing it in the URL
    field, for now.

---
+++ Test projects/Construct 2/Simple Platformer/output-html5[Simple Platformer]/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1772978724,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/goal-sheet0.png",
+		"images/death-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
